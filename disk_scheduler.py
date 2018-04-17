bdf1fa46d43e58b5f8d2c8598fbba9152211cf7f
Ram <ramhari.dahal1@gmail.com>
2018-04-17 00:34:45 -0600

    moves added

--- disk_scheduler.py
+++ disk_scheduler.py
@@ -5,17 +5,25 @@ requests = [2069, 1212, 2296, 2800, 544, 1618, 356, 1523, 4965, 3681]
 
 print("\nThe total distance the disk arm moves for each algorithm is as follows:")
 print("---------------------------")
- 
+
 def fcfs(requests):
 	distance = 0
 	local_head = head
+	moves = []
 	
 	for r in requests:
-		distance += abs(local_head - r)
+		move = abs(local_head - r)
+		moves.append(move)
+		distance += move
 		local_head = r
+	
+	moves = [str(a) for a in moves]
+	print ('{} {}'.format('Total Moves: ', ','.join(moves)))
+	
 	return distance
 
-print ('{} {}'.format("FCFS:", fcfs(requests)))
+print ('{} {}'.format("FCFS total:", fcfs(requests)))
+print("---------------------------")
 
 def sstf(requests):
 	local_head = head
@@ -24,6 +32,7 @@ def sstf(requests):
 	res = 0
 	max_d = max(tmp_req)
 	min_d = abs(local_head - max_d)
+	moves = []
 
 	while(len(tmp_req) > 0):
 		for r in tmp_req:
@@ -34,14 +43,20 @@ def sstf(requests):
 
 		local_head = max_d
 		tmp_req.remove(max_d)
+		moves.append(min_d)
 		distance += min_d
 
 		if (len(tmp_req) > 0):
 			min_d = abs(local_head - max(tmp_req))
 			max_d = max(tmp_req)
+	
+	moves = [str(a) for a in moves]
+	print ('{} {}'.format('Total Moves: ', ','.join(moves)))
+		
 	return distance
 	
-print ('{} {}'.format("SSTF:", sstf(requests)))
+print ('{} {}'.format("SSTF total:", sstf(requests)))
+print("---------------------------")
 
 def scan(requests):
 	local_head = head
@@ -50,10 +65,13 @@ def scan(requests):
 	
 	max_d = max(tmp_req)
 	end = 4999
+	moves = []
 	
 	for i in range(local_head, max_d+1):
 		if (i in tmp_req):
-			distance += abs(local_head - i)
+			move = abs(local_head - i)
+			moves.append(move)
+			distance += move
 			local_head = i
 			tmp_req.remove(i)
 
@@ -63,13 +81,20 @@ def scan(requests):
 	#tmp_c = max_d
 	while max_d >= 0:
 		if (max_d in tmp_req):
-			distance += abs(local_head - max_d)
+			move = abs(local_head - max_d)
+			moves.append(move)
+			distance += move
 			local_head = max_d
 			tmp_req.remove(max_d)
 		max_d -= 1
+
+	moves = [str(a) for a in moves]
+	print ('{} {}'.format('Total Moves: ', ','.join(moves)))
+	
 	return distance
 	
-print ('{} {}'.format("SCAN:", scan(requests)))
+print ('{} {}'.format("SCAN total:", scan(requests)))
+print("---------------------------")
 
 def look(requests):
 	local_head = head
@@ -77,22 +102,32 @@ def look(requests):
 	tmp_req = copy.copy(requests)
 	
 	max_d = max(tmp_req)
+	moves = []
 	
 	for i in range(local_head, max_d+1):
 		if (i in tmp_req):
-			distance += abs(local_head - i)
+			move = abs(local_head - i)
+			moves.append(move)
+			distance += move
 			local_head = i
 			tmp_req.remove(i)
 
 	while max_d >= 0:
 		if (max_d in tmp_req):
-			distance += abs(local_head - max_d)
+			move = abs(local_head - max_d)
+			moves.append(move)
+			distance += move
 			local_head = max_d
 			tmp_req.remove(max_d)
 		max_d -= 1
+	
+	moves = [str(a) for a in moves]
+	print ('{} {}'.format('Total Moves: ', ','.join(moves)))
+		
 	return distance	
 
-print ('{} {}'.format("LOOK:", look(requests)))
+print ('{} {}'.format("LOOK total:", look(requests)))
+print("---------------------------")
 
 def cscan(requests):
 	local_head = head
@@ -101,23 +136,35 @@ def cscan(requests):
 	
 	max_d = max(tmp_req)
 	end = 4999
+	moves = []
 	
 	for i in range(local_head, max_d+1):
 		if (i in tmp_req):
-			distance += abs(local_head - i)
+			move = abs(local_head - i)
+			moves.append(move)
+			distance += move
 			local_head = i
 			tmp_req.remove(i)
 
-	distance += abs(local_head - end)
+	move = abs(local_head - end)
+	moves.append(move)
+	distance += move
 	local_head = 0
 	
 	for i in range(0, max_d+1):
 		if (i in tmp_req):
-			distance += abs(local_head - i)
+			move = abs(local_head - i)
+			moves.append(move)
+			distance += move
 			local_head = i
 			tmp_req.remove(i)
+
+	moves = [str(a) for a in moves]
+	print ('{} {}'.format('Total Moves: ', ','.join(moves)))
+		
 	return distance
-print ('{} {}'.format("C-SCAN:", cscan(requests)))	
+print ('{} {}'.format("C-SCAN total:", cscan(requests)))
+print("---------------------------")	
 
 def clook(requests):
 	local_head = head
@@ -125,19 +172,28 @@ def clook(requests):
 	tmp_req = copy.copy(requests)
 	
 	max_d = max(tmp_req)
+	moves = []
 	
 	for i in range(local_head, max_d+1):
 		if (i in tmp_req):
-			distance += abs(local_head - i)
+			move = abs(local_head - i)
+			moves.append(move)
+			distance += move
 			local_head = i
 			tmp_req.remove(i)
 	
 	local_head = min(tmp_req)
 	for i in range(0, max_d+1):
 		if (i in tmp_req):
-			distance += abs(local_head - i)
+			move = abs(local_head - i)
+			moves.append(move)
+			distance += move
 			local_head = i
 			tmp_req.remove(i)
+	moves = [str(a) for a in moves]
+	print ('{} {}'.format('Total Moves: ', ','.join(moves)))
+	
 	return distance
-print ('{} {}'.format("C-LOOK:", clook(requests)))
+print ('{} {}'.format("C-LOOK total:", clook(requests)))
+print("---------------------------")
 
